397086214409bed9d3833a0d828f47826cfe4933
Zelimkhan Khalidov <ondalom@gmail.com>
2016-04-21 16:12:42 +0300

    make sense

---
+++ index.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var fs = require('fs');
+var socketio = require('socket.io');
+var html = require('escape-html');
+
+var server = http.createServer();
+var io = socketio(server);
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }   
+    server.on('request', function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.end(request.url == '/' ? html : '');  
+    });
+
+    server.listen(8000);
+});
+
+io.on('connection', function(socket){
+
+  socket.on('message', function(data) {
+  	if(data && typeof data.nickname == 'string' && typeof data.message == 'string' && data.nickname && data.message) {
+  		socket.broadcast.emit('message', { nickname: html(data.nickname), message: html(data.message) });
+  	}
+  })
+
+});
